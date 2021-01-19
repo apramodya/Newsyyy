41ff7a41305b1f125abf5aeef17f74b6f3c1b23f
Pramodya Abeysinghe <pramodyaabeysinghe@gmail.com>
2021-01-19 23:54:17 +0530

    basic ui for settings view

--- Newsyyy/Views/SettingsView.swift
+++ Newsyyy/Views/SettingsView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct SettingsView: View {
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    
     var body: some View {
-        Text("Settings")
+        NavigationView {
+            List {
+                VStack(alignment: .leading, spacing: 20, content: {
+                    Text("About").font(.title)
+                    
+                    Button("Privacy Policy") {
+                        print("Privacy Policy")
+                    }.foregroundColor(.blue)
+                    
+                    Button("Terms and Conditions") {
+                        print("Terms and Conditions")
+                    }.foregroundColor(.blue)
+                })
+                
+                VStack(alignment: .leading, spacing: 20, content: {
+                    Text("App").font(.title)
+                    HStack(alignment: .center, spacing: 0, content: {
+                        Text("v").bold()
+                        Text(appVersion ?? "N/A")
+                    })
+                })
+            }.navigationTitle("Settings")
+            .listStyle(SidebarListStyle())
+        }
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        Group {
+            SettingsView()
+        }
     }
 }
