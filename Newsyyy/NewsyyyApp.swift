a28851a6d54833f472d78ca7fe7ccc97f4e63f29
Pramodya Abeysinghe <pramodyaabeysinghe@gmail.com>
2021-01-19 22:47:44 +0530

    init project

---
+++ Newsyyy/NewsyyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsyyyApp.swift
+//  Newsyyy
+//
+//  Created by Pramodya Abeysinghe on 2021-01-19.
+//
+
+import SwiftUI
+
+@main
+struct NewsyyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
